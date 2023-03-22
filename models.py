3eece858a4a2a2ee75d46d39531c754712410069
Ilona Kowalewska <ilona.kowalewska112@gmail.com>
2023-03-22 18:28:36 +0100

    Added users table

--- models.py
+++ models.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+
 from sqlalchemy import (
-    create_engine
+    create_engine, Column, String, Integer, DateTime
 )
+
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
     "mysql+pymysql://root:root@127.0.0.1:3306/portal"
 )
 Base = declarative_base(bind=engine)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    fullname = Column(String(50))
+    lastname = Column(String(50))
+    nickname = Column(String(50), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False )
+    registration_date = Column(DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"User({self.nickname}"
